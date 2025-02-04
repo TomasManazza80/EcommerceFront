56c661ba64f781d892d948796bcf76b4ff311136
Tomás Augusto Manazza <161159185+TomasManazza80@users.noreply.github.com>
2025-02-04 15:18:04 -0300

    Update index.jsx

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -16,7 +16,7 @@ const Products = () => {
 
   async function fetchProducts() {
     try {
-      const { data } = await axios.get(`${API_URL}/products`);
+      const { data } = await axios.get(`https://ecommerceback-haed.onrender.com/products`);
       const sortedData = data.sort(compareName);
       setProduct(sortedData);
       setFilterArray(sortedData);
