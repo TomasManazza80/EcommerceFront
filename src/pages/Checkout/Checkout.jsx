0dd386539430742a88df964a98734e80785ddc3b
Tomás Augusto Manazza <161159185+TomasManazza80@users.noreply.github.com>
2025-02-04 15:05:25 -0300

    Update Checkout.jsx

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_URL = 'https://ecommerceback-server.onrender.com';
+const API_URL = 'https://ecommerceback-haed.onrender.com';
 
 
 
