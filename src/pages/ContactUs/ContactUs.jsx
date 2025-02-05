7e05eec04008215109ca031fc7c5d2ad316e08e8
Tomás Augusto Manazza <161159185+TomasManazza80@users.noreply.github.com>
2025-02-05 19:23:44 -0300

    Update ContactUs.jsx

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -6,8 +6,9 @@ function ContactUs() {
     <div className="mt-20 w-60 m-auto items-center text-center ">
       <br />
       <h1 className="text-4xl text-center">Contacto</h1>
-      <p className="p-4">Email: abs@gmail.com</p>
-      <p className="p-4">Cell: +54 123 456 789</p>
+      <p className="p-4">Email: solcito26wp@gmail.com</p>
+      <p className="p-4">Cell: 342-5048139</p>
+      <p className="p-4">Instagram: holanam_nam</p>
 
       <NavLink to="/" className="p-1 border">
         Go Back
